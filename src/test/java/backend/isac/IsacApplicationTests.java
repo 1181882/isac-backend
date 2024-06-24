8a4f630f6213e14336946b046d750ae46d5d5634
Rafael Soares <1181882@isep.ipp.pt>
2024-06-24 20:56:56 +0100

    Spring Initializer

---
+++ src/test/java/backend/isac/IsacApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.isac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
